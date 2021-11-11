077030a382d324ca2af202ce6746fdddb5803be6
Saeed Rahimi Manesh <rahimimanesh.saeeid@gmail.com>
2021-11-12 01:35:43 +0330

    posts routes started - end of s1

---
+++ routes/web.go
@@ -0,0 +1,49 @@
+package routes
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Post struct {
+	Id    int    `json:"id"`
+	Title string `json:"title"`
+	Text  string `json:"text"`
+}
+
+var (
+	posts []Post
+)
+
+func init() {
+	posts = []Post{Post{Id: 1, Title: "Title 1", Text: "Text 1"}}
+}
+
+// GetPosts /*
+func GetPosts(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-type", "application/json")
+	result, err := json.Marshal(posts)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"error": "Error marshalling the posts array"}`))
+		return
+	}
+	response.WriteHeader(http.StatusOK)
+	response.Write(result)
+}
+
+func AddPost(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-type", "application/json")
+	var post Post
+	err := json.NewDecoder(request.Body).Decode(&post)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"error": "Error marshalling the posts array"}`))
+		return
+	}
+	post.Id = len(posts) + 1
+	posts = append(posts, post)
+	response.WriteHeader(http.StatusOK)
+	result, err := json.Marshal(post)
+	response.Write(result)
+}
--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	"golang-rest-api/routes"
 	"log"
 	"net/http"
 )
@@ -11,10 +12,11 @@ const port string = ":8000"
 
 func main() {
 	router := mux.NewRouter()
-
 	router.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
 		fmt.Fprintln(response, "Up and running...")
 	})
+	router.HandleFunc("/posts", routes.GetPosts).Methods("GET")
+	router.HandleFunc("/posts/add", routes.AddPost).Methods("POST")
 	log.Println("Server Listening on port", port)
 	log.Fatalln(http.ListenAndServe(port, router))
 }
